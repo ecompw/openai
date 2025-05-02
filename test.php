b30e956f6149aa32f63101da7cc9d5d258d69bb7
max <m.safianov@gmail.com>
2025-05-02 20:35:22 +0400

    correct test

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/includes/plugin-update-checker-master/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 if (class_exists('PucFactory')) {
     echo "PucFactory class loaded successfully!";
